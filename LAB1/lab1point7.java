96e922960a369cda03a59ea35dc45708bca2c670
Aungkanr <augkar1234@gmail.com>
2025-11-22 11:22:11 +0700

    EIEI

--- LAB1/lab1point7.java
+++ LAB1/lab1point7.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
     public class lab1point7 {
         public static void main(String[] args) {
-            
             Scanner input = new Scanner(System.in);
-            System.out.print("Please value");
-            int mul = input.nextInt();
-            
+            System.out.print(" value = "); 
+            int num = input.nextInt(); 
+            int sum = 0;
 
-
-
-
-
+            for(int value = 1 ; value <= num ; value++ ){ 
+                int numbee = input.nextInt();
+                sum += numbee ;
+        
+            }
+            System.out.println("answer = "+sum);
             input.close();
         }
-    
-}
+    }
---
+++ lab1point8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class lab1point8 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Please Enter Number ");
+        int ar=input.nextInt();
+        int i = 0 ;
+
+        for (int number = 1 ;   )
+
+
+    }
+}
