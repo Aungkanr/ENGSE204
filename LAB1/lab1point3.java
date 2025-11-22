c07ff496566e2dabc2756a66b3d5f1ce8225de01
Aungkanr <augkar1234@gmail.com>
2025-11-22 17:46:20 +0700

    EIEI

--- LAB1/lab1point3.java
+++ LAB1/lab1point3.java
@@ -3,7 +3,7 @@ import java.util.Scanner ;
     public static void main(String[] exam) {
     
         Scanner eo=new Scanner(System.in);
-            System.out.print(" enter number = ");
+            System.out.print(" enter num = ");
             int number=eo.nextInt();
             if(number%2 == 0 ){
                 System.out.println(" Even ");
