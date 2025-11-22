5f2acd8c91d87e55f4700c2fac145d8ef9080ef0
Aungkanr <augkar1234@gmail.com>
2025-11-22 22:10:25 +0700

    EIEI

---
+++ lab1point10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class lab1point10 {
+    public static void main(String[] wihe) {
+        Scanner input=new Scanner (System.in);
+        
+        System.out.print("width value : ");
+        double wid = input.nextDouble();
+
+        System.out.print("high value : ");
+        double high = input.nextDouble();
+
+        double Average = calculatearea(wid , high);
+
+        System.out.println(Average);
+
+
+    }
+     public static double calculatearea(double wid, double high) {
+        Scanner num = new Scanner(System.in);
+
+            double area = high * wid ;
+    
+            return area;
+     }
+}
+
+
---
+++ lab1point9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class lab1point9 {
+    public static void main(String[] rage) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print(" value :");
+        int value = input.nextInt();
+        int[] sum = new int[value];
+        double Average = 0;
+
+        for (int A = 0 ; A < value; A++){
+            sum [A] = input.nextInt();
+                Average += sum [A];             
+
+            }
+        
+        System.out.println(" Average result  = " + Average / value );
+        input.close();
+    }
+}
+    
