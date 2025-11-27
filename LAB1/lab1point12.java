e79cbec4996d15ddbadb7bae71aa3e1704addb4c
Aungkanr <augkar1234@gmail.com>
2025-11-27 22:05:19 +0700

    EIEI

--- LAB1/lab1point12.java
+++ LAB1/lab1point12.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class lab1point12 {
-    public static void main(String[] mange) {
+    public static void main(String[] mast) {
+
         Scanner input = new Scanner(System.in);
         System.out.print(" tok kansai ki sanid  : ");
         int magm = input.nextInt();
@@ -8,22 +9,23 @@ public class lab1point12 {
         int[] ProductId = new int[magm];
         int[] Stock = new int[magm];
 
-        for (int M = 0 ; M < magm ; M++){
-            System.out.println("Sai Ai Di : " +(M+1));
-            ProductId[M] = input.nextInt();
+        for (int s = 0 ; s < magm ; s++){
+            System.out.println("Sai Ai Di : " +(s+1));
+            ProductId[s] = input.nextInt();
 
-            System.out.println("Sai sa tok : " +(M+1));
-            Stock[M] = input.nextInt();
+            System.out.println("Sai sa tok : " +(s+1));
+            Stock[s] = input.nextInt();
         }
         
         System.out.println("Sai Id ti thong kan ha");
         int lek = input.nextInt();
 
-        int M;
+        int m;
+
         boolean  found = false;
-        for ( M = 0; M < magm; M++ )  {
-            if (lek == ProductId[M]){
-                System.out.printf(" mi khong nai Stock %d Piece : ",Stock[M]);
+        for ( m = 0; m < magm; m++ )  {
+            if (lek == ProductId[m]){
+                System.out.printf(" mi khong nai Stock %d Piece : ",Stock[m]);
                 found = true;
                 break;
             }
